c8d1b6380b3c88a3f7cb3047710d931b6d4adf86
redroc <38569066+redroc@users.noreply.github.com>
2019-02-21 00:17:06 +0100

    added solution for problem 10

---
+++ 10/main.c
@@ -0,0 +1,42 @@
+/*What is the sum of all primes below two million?*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAGIC 2000000
+
+void print(unsigned long * primes, unsigned long numtoprint) {
+	for(unsigned long i = 0; i < numtoprint; ++i)
+		if(primes[i])
+			printf("%zd ",i);
+}
+
+int main(){
+	unsigned long long sum = 0; 
+	unsigned long * primes;
+
+	primes = (unsigned long *) calloc(MAGIC,sizeof(unsigned long));
+
+	/* 0 stands for: Not a prime number.
+	 * 1 stands for: Is a prime number. */
+
+	for (unsigned long i = 0; i < MAGIC; primes[i++] = 1)  
+		;
+	
+	/* Generate prime number array. */
+	primes[0] = 0;
+	primes[1] = 0;
+	for (unsigned long i = 2; i < MAGIC; ++i)
+		for (unsigned long j = 2; j * i <= MAGIC; ++j)
+			primes[j * i] = 0;
+	 
+	/*Calculate sum */
+	for (unsigned long long i = 0; i < MAGIC; ++i)  
+		sum += primes[i] * i;
+
+	printf("The sum of all primes below two million is: %llu\n",
+		   sum);
+	free(primes);
+	return 0;
+}
