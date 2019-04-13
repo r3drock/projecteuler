383d26b8d4e0e28f8908611552f6fbfa918d0660
redroc <38569066+redroc@users.noreply.github.com>
2019-04-14 00:00:02 +0200

    removed unnecessary divisors in problem 5

--- 5/main.c
+++ 5/main.c
@@ -8,25 +8,16 @@ int main()
 {
 	unsigned long long number = 2520;
 
-	while (!(number %  2 == 0 &&
-		     number %  3 == 0 &&
-		     number %  4 == 0 &&
-		     number %  5 == 0 &&
-		     number %  6 == 0 &&
-		     number %  7 == 0 &&
-		     number %  8 == 0 &&
-		     number %  9 == 0 &&
-		     number % 10 == 0 &&
-		     number % 11 == 0 &&
-		     number % 12 == 0 &&
-		     number % 13 == 0 &&
-		     number % 14 == 0 &&
-		     number % 15 == 0 &&
-		     number % 16 == 0 &&
-		     number % 17 == 0 &&
-		     number % 18 == 0 &&
+	while (!(number % 20 == 0 &&
 		     number % 19 == 0 &&
-		     number % 20 == 0)) 
+		     number % 18 == 0 &&
+		     number % 17 == 0 &&
+		     number % 16 == 0 &&
+		     number % 15 == 0 &&
+		     number % 14 == 0 &&
+		     number % 13 == 0 &&
+		     number % 12 == 0 &&
+		     number % 11 == 0))
 		++number;
 
 	printf("The smallest positive number that is evenly divisible\n\
