bb98999d450ea06320c92496546e77844f0fbcc0
redroc <38569066+redroc@users.noreply.github.com>
2019-05-12 17:37:32 +0200

    initial commit for problem 28

---
+++ 28/main.c
@@ -0,0 +1,22 @@
+/* What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way? */
+
+#include "stdio.h"
+
+void buildSpiral(unsigned int size, unsigned int **spiral);
+
+unsigned long long calculateDiagonalSum();
+
+int main()
+{
+
+	return 0;
+}
+
+void buildSpiral(unsigned int size, unsigned int **spiral)
+{
+}
+
+unsigned long long calculateDiagonalSum()
+{
+	return 0;
+}
