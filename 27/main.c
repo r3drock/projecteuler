89bc0eadd125834a86cc7952363e7010edbe6dd6
redroc <38569066+redroc@users.noreply.github.com>
2019-05-26 00:00:17 +0200

    add solution for problem 27

--- 27/main.c
+++ 27/main.c
@@ -10,55 +10,57 @@
 #define BLIM 1000
 #define MAXPRIME 1000*2000+1001
 
-void print(unsigned long * primes, unsigned long numtoprint) 
+void print( long * primes,  long numtoprint) 
 {
-	for(unsigned long i = 0; i < numtoprint; ++i)
+	for( long i = 0; i < numtoprint; ++i)
 		if(primes[i])
 			printf("%zd ",i);
 }
 
-unsigned long * buildPrimes()
+long * buildPrimes()
 {
-	unsigned long * primes;
+	long * primes;
 
-	primes = (unsigned long *) calloc(MAXPRIME,sizeof(unsigned long));
+	primes = (long *) calloc(MAXPRIME,sizeof(long));
 
 	/* 0 stands for: Not a prime number.
 	 * 1 stands for: Is a prime number. */
 
-	for (unsigned long i = 0; i < MAXPRIME; primes[i++] = 1)  
+	for ( long i = 0; i < MAXPRIME; primes[i++] = 1)  
 		;
-	
+
 	/* Generate prime number array. */
 	primes[0] = 0;
 	primes[1] = 0;
-	for (unsigned long i = 2; i < MAXPRIME; ++i)
-		for (unsigned long j = 2; j * i <= MAXPRIME; ++j)
+	for ( long i = 2; i < MAXPRIME; ++i)
+		for ( long j = 2; j * i <= MAXPRIME; ++j)
 			primes[j * i] = 0;
+
+	return primes;
 }
 
-unsigned long checkConsecutivePrimeNumbers(unsigned long * primes,
-		unsigned long a, unsigned long b)
+long checkConsecutivePrimeNumbers(long * primes,
+		long a, long b)
 {
-	unsigned long n = 0;
+	long n = 0;
 	if (((n * n) + (a * n) + b) < 0)
 		return 0;
-	while (primes[(n * n) + (a * n) + b])
+
+	while ((((n * n) + (a * n) + b) > 0 ) && (primes[(n * n) + (a * n) + b]))
 		++n;
 	return n;
 }
 
 int main()
 {
-	unsigned long * primes = buildPrimes();
-	unsigned long long product = 0;
-	unsigned long longestConsecutivePrimeNumbers = 0;
-	unsigned long besta = 0;
-	unsigned long bestb = 0;
+	long * primes = buildPrimes();
+	long longestConsecutivePrimeNumbers = 0;
+	long besta = 0;
+	long bestb = 0;
 
-	for (unsigned long a = -999; a < 1000; ++a) {
-		for (unsigned long b = -999; b < 1000; ++b) {
-			unsigned long temp = checkConsecutivePrimeNumbers(primes, a, b);
+	for (long a = -999; a < 1000; ++a) {
+		for (long b = -999; b < 1000; ++b) {
+			long temp = checkConsecutivePrimeNumbers(primes, a, b);
 			if (temp > longestConsecutivePrimeNumbers) {
 				longestConsecutivePrimeNumbers = temp;
 				besta = a;
@@ -67,7 +69,8 @@ int main()
 		}
 	}
 
-	printf("The product of the coefficients is: %llu\n", besta * bestb);
+	printf("The product of the coefficients a: %ld, b: %ld is: %lld\nIt were %ld consecutive primes.\n",
+			besta, bestb, besta * (long long) bestb, longestConsecutivePrimeNumbers);
 	free(primes);
 	return 0;
 }
