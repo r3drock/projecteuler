f560c232c6df28a45c9c4263e7b9b67087673690
r3drock <philipp.koppenstein@udo.edu>
2019-07-11 22:14:00 +0200

    begin implementation of problem 31

---
+++ 31/main.c
@@ -0,0 +1,17 @@
+/* How many different ways can £2 be made using any number of coins? */
+
+#include <stdio.h>
+
+#define numcoins 8
+
+const int amount = 200;
+const int coins[numcoins] = {200, 100, 50, 20, 10, 5, 2, 1};
+
+int main()
+{
+	int count = 0;
+
+	printf("count: %d\n", count);
+	return 0;
+}
+
